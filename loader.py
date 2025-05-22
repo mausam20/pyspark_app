64aa8e7eb01c5c25ae0ff64d86ca146877198485
aastha <20aasthaprasad@gmail.com>
2025-05-21 21:48:08 -0700

    first commit

---
+++ loader.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# MAGIC %run "./loaderfactory"
+
+# COMMAND ----------
+
+class AbstractLoader:
+    def __init__(self, transformedDF):
+        self.transformedDF = transformedDF
+
+    def sink(self):
+
+        pass
+
+class AirPodsAfterIphoneLoader(AbstractLoader):
+
+    def sink(self):
+        get_sink_source(
+            sink_type = "dbfs",
+            df = self.transformedDF, 
+            path = "dbfs:/FileStore/tables/apple_analysis/output/airpodsAfterIphone", 
+            method = "overwrite"
+        ).load_data_frame()
+
+class OnlyAirpodsAndIPhoneLoader(AbstractLoader):
+
+    def sink(self):
+        params = {
+            "partitionByColumns": ["location"]
+        }
+        get_sink_source(
+            sink_type = "dbfs_with_partition",
+            df = self.transformedDF, 
+            path = "dbfs:/FileStore/tables/apple_analysis/output/airpodsOnlyIphone", 
+            method = "overwrite",
+            params = params
+        ).load_data_frame()
+
+        get_sink_source(
+            sink_type = "delta",
+            df = self.transformedDF, 
+            path = "default.onlyAirPodsAndIphone", 
+            method = "overwrite",
+        ).load_data_frame()           
---
+++ transformer.py
@@ -0,0 +1,108 @@
+# Databricks notebook source
+from pyspark.sql.window import Window
+from pyspark.sql.functions import lead, col, broadcast, collect_set, size, array_contains
+
+
+class Transformer:
+    def __init__(self):
+        pass
+
+    def transform(self, inputDFs):
+        pass
+
+class AirpodsAfterIphoneTransformer(Transformer):
+
+    def transform(self, inputDFs):
+        """
+        Customers who have bought Airpods after buying the iPhone
+        """
+
+        transcatioInputDF = inputDFs.get("transcatioInputDF")
+
+        print("transcatioInputDF in transform")
+
+        transcatioInputDF.show()
+
+        windowSpec = Window.partitionBy("customer_id").orderBy("transaction_date")
+
+        transformedDF = transcatioInputDF.withColumn(
+            "next_product_name", lead("product_name").over(windowSpec)
+        )
+
+        print("Airpods after buying iphone")
+        transformedDF.orderBy("customer_id", "transaction_date", "product_name").show()
+
+        filteredDF = transformedDF.filter(
+            (col("product_name") == "iPhone") & (col("next_product_name") == "AirPods")
+        )
+
+        filteredDF.orderBy("customer_id", "transaction_date", "product_name").show()
+
+        customerInputDF = inputDFs.get("customerInputDF")
+
+        customerInputDF.show()
+
+        joinDF =  customerInputDF.join(
+           broadcast(filteredDF),
+            "customer_id"
+        )
+
+        print("JOINED DF")
+        joinDF.show()
+
+        return joinDF.select(
+            "customer_id",
+            "customer_name",
+            "location"
+        )
+
+
+class OnlyAirpodsAndIphone(Transformer):
+
+    def transform(self, inputDFs):
+        """
+        Customer who have bought only iPhone and Airpods nothing else
+        """
+
+        transcatioInputDF = inputDFs.get("transcatioInputDF")
+
+        print("transcatioInputDF in transform")
+
+        groupedDF = transcatioInputDF.groupBy("customer_id").agg(
+            collect_set("product_name").alias("products")
+        )
+
+        print("Grouped DF")
+        groupedDF.show()
+
+        filteredDF = groupedDF.filter(
+            (array_contains(col("products"), "iPhone")) &
+            (array_contains(col("products"), "AirPods")) & 
+            (size(col("products")) == 2)
+        )
+        
+        print("Only Airpods and iPhone")
+        filteredDF.show()
+
+        customerInputDF = inputDFs.get("customerInputDF")
+
+        customerInputDF.show()
+
+        joinDF =  customerInputDF.join(
+           broadcast(filteredDF),
+            "customer_id"
+        )
+
+        print("JOINED DF")
+        joinDF.show()
+
+        return joinDF.select(
+            "customer_id",
+            "customer_name",
+            "location"
+        )
+
+
+
+
+
